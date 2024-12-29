bb6a41458ab6668f415841e7a3e64079d7374c4f
xshthkr <shashankt2004@gmail.com>
2024-12-29 08:11:13 +0530

    update client.c

---
+++ src/client.c
@@ -0,0 +1,19 @@
+#include <protocol.h>
+
+#include <stdio.h>
+
+#define PORT 12345
+#define SERVER_ADDR "127.0.0.1"
+#define MAX_BUF_LEN 1024
+
+int main() {
+
+    Sender sender;
+    
+    create_sender_socket(&sender, PORT, SERVER_ADDR);
+
+    start_handshake(&sender);
+
+    return 0;
+}
+
