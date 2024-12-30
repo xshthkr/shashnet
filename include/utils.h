f7cc5d4a9fd0d6fd3c757872bc2207ed0f8fe114
xshthkr <shashankt2004@gmail.com>
2024-12-30 15:56:19 +0530

    queue datastructure init

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,25 @@
 #ifndef utils
 #define utils
 
+#include <protocol.h>
+
 #include <stdint.h>
 
+typedef struct {
+    Packet *packets;
+    uint8_t front;
+    uint8_t rear;
+} Queue;
+
 uint16_t checksum(const char *data, int len);
+
+int init_queue(Queue *queue);
+int enqueue(Queue *queue, Packet *packet);
+int dequeue(Queue *queue, Packet *packet);
+int is_empty(Queue *queue);
+int is_full(Queue *queue);
+Packet *front(Queue *queue);
+Packet *rear(Queue *queue);
+void print_queue(Queue *queue);
 
 #endif // utils
