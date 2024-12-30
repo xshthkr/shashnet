637a55843ae24609de9f3241ceacafd43031b285
xshthkr <shashankt2004@gmail.com>
2024-12-30 15:49:41 +0530

    rdt 2.0

--- src/transport.c
+++ src/transport.c
@@ -18,15 +18,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 
 // client sends packet to server
-// client waits for ACK
 int send_packet_client(ShashnetClient *sender, char *message) {
+
+    /* state 1: waiting for call to send data */
 
     // make packet
     Packet packet;
-    init_packet(&packet, sender->seq_num, sender->ack_num, message);
+    init_packet(&packet, sender->seq_num, sender->ack_num, message);    // checksum computed internally
     
     // send packet
     sendto(sender->sockfd, &packet, sizeof(Packet), 0, (struct sockaddr *) &sender->server_addr, sender->server_addr_len);
@@ -36,20 +38,67 @@ int send_packet_client(ShashnetClient *sender, char *message) {
     printf("Sent DATA packet\n");
     print_packet(&packet);
 
+    /* state 2: waiting for ACK or NAK packet */
+
+    while (1) {
+        
+        // receive packet
+        Packet ackpacket;
+        recvfrom(sender->sockfd, &ackpacket, sizeof(Packet), 0, NULL, NULL);
+
+        // debug
+        printf("Received ACK/NAK packet\n");
+        print_packet(&ackpacket);
+
+        // if ACK received then break out of loop, else resend packet
+        if (ackpacket.ack_num == sender->seq_num) {
+            break;
+        } else {
+            
+            // debug
+            printf("Resending DATA packet\n");
+
+            sendto(sender->sockfd, &packet, sizeof(Packet), 0, (struct sockaddr *) &sender->server_addr, sender->server_addr_len);
+        }
+    }
+
     return 0;
 }
 
 // server receives packet
-// server ACKs client
 int recv_packet_server(ShashnetServer *receiver, char *message) {
     
+    /* state 1: receive packet from client */
+
     // receive packet
     Packet packet;
     recvfrom(receiver->sockfd, &packet, sizeof(Packet), 0, (struct sockaddr *) &receiver->client_addr, &receiver->client_addr_len);
-    // validate_packet_checksum(packet)
 
-    // extract message
-    strcpy(message, packet.payload);
+    // debug
+    printf("Received DATA packet\n");
+    print_packet(&packet);
+
+    // if packet is valid, send ACK, else send NAK
+    if (validate_packet_checksum(&packet)) {
+        // extract message from packet
+        strcpy(message, packet.payload);
+        receiver->ack_num = packet.seq_num + 1;
+        init_packet(&packet, receiver->seq_num, receiver->ack_num, "ACK");
+        sendto(receiver->sockfd, &packet, sizeof(Packet), 0, (struct sockaddr *) &receiver->client_addr, receiver->client_addr_len);
+    
+        // debug
+        printf("Sent ACK packet\n");
+        print_packet(&packet);
+    
+    } else {
+        init_packet(&packet, receiver->seq_num, receiver->ack_num, "NAK");
+        sendto(receiver->sockfd, &packet, sizeof(Packet), 0, (struct sockaddr *) &receiver->client_addr, receiver->client_addr_len);
+    
+        // debug
+        printf("Sent NAK packet\n");
+        print_packet(&packet);
+    
+    }
 
     return 0;
 }
