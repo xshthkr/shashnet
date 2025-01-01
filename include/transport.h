3e6d49826a0b427a5578797d986d8f8cfc0e3735
xshthkr <shashankt2004@gmail.com>
2025-01-01 18:58:17 +0530

    rename param

--- include/transport.h
+++ include/transport.h
@@ -4,7 +4,7 @@
 #include <transport.h>
 #include <protocol.h>
 
-int send_packet_client(ShashnetClient *sender, char *message);
-int recv_packet_server(ShashnetServer *receiver, char *message);
+int send_packet_client(ShashnetClient *sender, char *data);
+int recv_packet_server(ShashnetServer *receiver, char *data);
 
 #endif // transport
